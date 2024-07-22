40233396f043b85955ddfdd0dd53fda0c05a5826
Maxik-web <kleok0039@gmail.com>
2024-07-22 20:45:16 +0300

    add

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'), // Ваш основной файл
-        second: resolve(__dirname, 'car.html'), // Второй файл HTML
+        car: resolve(__dirname, 'car.html'), // Второй файл HTML
+        about: resolve(__dirname, 'about.html'), // Второй файл HTML
+        business: resolve(__dirname, 'business.html'), // Второй файл HTML
+        contacts: resolve(__dirname, 'contacts.html'), // Второй файл HTML
+        license: resolve(__dirname, 'license.html'), // Второй файл HTML
+        partner: resolve(__dirname, 'partner.html'), // Второй файл HTML
       },
     },
   },
